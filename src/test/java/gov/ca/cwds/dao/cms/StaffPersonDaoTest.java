ce296849a1cdf506bb619a91f626302f178350fd
David R. Smith <David.Smith@osi.ca.gov>
2018-03-08 09:02:42 -0800

    SonarQube, Javadoc

--- src/test/java/gov/ca/cwds/dao/cms/StaffPersonDaoTest.java
+++ src/test/java/gov/ca/cwds/dao/cms/StaffPersonDaoTest.java
@@ -1,13 +1,63 @@
 package gov.ca.cwds.dao.cms;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.CacheMode;
+import org.hibernate.FlushMode;
+import org.hibernate.ScrollMode;
+import org.hibernate.ScrollableResults;
+import org.hibernate.query.Query;
+import org.hibernate.type.StringType;
+import org.hibernate.type.TimestampType;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
+import gov.ca.cwds.data.persistence.cms.StaffPerson;
 import gov.ca.cwds.jobs.Goddard;
 
 public class StaffPersonDaoTest extends Goddard {
+
+  StaffPersonDao target;
+
+  @Override
+  @Before
+  public void setup() throws Exception {
+    super.setup();
+
+    target = new StaffPersonDao(sessionFactory);
+    final Query q = Mockito.mock(Query.class);
+
+    when(sessionFactory.getCurrentSession()).thenReturn(session);
+    when(session.getNamedQuery(any())).thenReturn(q);
+    when(q.list()).thenReturn(new ArrayList<>());
+
+    when(q.setString(any(String.class), any(String.class))).thenReturn(q);
+    when(q.setParameter(any(String.class), any(String.class), any(StringType.class))).thenReturn(q);
+    when(q.setHibernateFlushMode(any(FlushMode.class))).thenReturn(q);
+    when(q.setReadOnly(any(Boolean.class))).thenReturn(q);
+    when(q.setCacheMode(any(CacheMode.class))).thenReturn(q);
+    when(q.setFlushMode(any(FlushMode.class))).thenReturn(q);
+    when(q.setFetchSize(any(Integer.class))).thenReturn(q);
+    when(q.setCacheable(any(Boolean.class))).thenReturn(q);
+    when(q.setParameter(any(String.class), any(Timestamp.class), any(TimestampType.class)))
+        .thenReturn(q);
+
+    final ScrollableResults results = mock(ScrollableResults.class);
+    when(q.scroll(any(ScrollMode.class))).thenReturn(results);
+    when(results.next()).thenReturn(true).thenReturn(false);
+    when(results.get()).thenReturn(new Object[0]);
+  }
 
   @Test
   public void type() throws Exception {
@@ -16,8 +66,14 @@ public class StaffPersonDaoTest extends Goddard {
 
   @Test
   public void instantiation() throws Exception {
-    StaffPersonDao target = new StaffPersonDao(sessionFactory);
     assertThat(target, notNullValue());
+  }
+
+  @Test
+  public void findAll_A$() throws Exception {
+    List<StaffPerson> actual = target.findAll();
+    List<StaffPerson> expected = new ArrayList<>();
+    assertThat(actual, is(equalTo(expected)));
   }
 
 }
