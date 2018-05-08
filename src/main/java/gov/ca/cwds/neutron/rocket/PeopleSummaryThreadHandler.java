79aef1f82ab048b9e9db2879ccb6270e2f35cfe3
David R. Smith <David.Smith@osi.ca.gov>
2018-05-07 18:37:56 -0700

    HOT-1885: People Summary: last known residence address (R-02294)

--- src/main/java/gov/ca/cwds/neutron/rocket/PeopleSummaryThreadHandler.java
+++ src/main/java/gov/ca/cwds/neutron/rocket/PeopleSummaryThreadHandler.java
@@ -103,13 +103,13 @@ public class PeopleSummaryThreadHandler implements ApiMarker, AtomLoadEventHandl
   protected void mapReplicatedClient(PlacementHomeAddress pha) {
     if (normalized.containsKey(pha.getClientId())) {
       final ReplicatedClient rc = normalized.get(pha.getClientId());
-      rc.addClientAddress(pha.toReplicatedClientAddress());
+      rc.setActivePlacementHomeAddress(pha);
     }
   }
 
   @Override
   public void eventJdbcDone(final Pair<String, String> range) {
-    // Merge placement home addresses HERE.
+    // Merge placement home addresses.
     this.placementHomeAddresses.values().stream().forEachOrdered(this::mapReplicatedClient);
 
     // Send to Elasticsearch.
