43de614fd5adf5a13c17237fd9a3da51237f10c4
David R. Smith <David.Smith@osi.ca.gov>
2018-09-21 13:51:45 -0700

    HOT-2579: Neutron record accounting

--- src/test/java/gov/ca/cwds/neutron/sqllite/SQLiteJDBC.java
+++ src/test/java/gov/ca/cwds/neutron/sqllite/SQLiteJDBC.java
@@ -2,6 +2,7 @@ package gov.ca.cwds.neutron.sqllite;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class SQLiteJDBC {
@@ -11,7 +12,6 @@ public class SQLiteJDBC {
       Class.forName("org.sqlite.JDBC");
     } catch (Exception e) {
       System.err.println(e.getClass().getName() + ": " + e.getMessage());
-      System.exit(0);
     }
   }
 
@@ -29,13 +29,73 @@ public class SQLiteJDBC {
        //@formatter:on
 
       stmt.executeUpdate(sql);
-      stmt.close();
+    } catch (Exception e) {
+      System.err.println(e.getClass().getName() + ": " + e.getMessage());
+      e.printStackTrace();
+    }
+
+    System.out.println("Table created successfully");
+  }
+
+  public void insert() {
+    try (final Connection c = connect(); final Statement stmt = c.createStatement()) {
+      c.setAutoCommit(false);
+      System.out.println("Opened database successfully");
+
+      String sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+          + "VALUES (1, 'Paul', 32, 'California', 20000.00 );";
+      stmt.executeUpdate(sql);
+
+      sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+          + "VALUES (2, 'Allen', 25, 'Texas', 15000.00 );";
+      stmt.executeUpdate(sql);
+
+      sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+          + "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );";
+      stmt.executeUpdate(sql);
+
+      sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+          + "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
+      stmt.executeUpdate(sql);
+
+      c.commit();
+    } catch (Exception e) {
+      System.err.println(e.getClass().getName() + ": " + e.getMessage());
+      e.printStackTrace();
+    }
+
+    System.out.println("Records created successfully");
+  }
+
+  public void select() {
+    try (final Connection c = connect(); final Statement stmt = c.createStatement()) {
+      c.setAutoCommit(false);
+      System.out.println("Opened database successfully");
+      final ResultSet rs = stmt.executeQuery("SELECT * FROM COMPANY;");
+
+      while (rs.next()) {
+        int id = rs.getInt("id");
+        String name = rs.getString("name");
+        int age = rs.getInt("age");
+        String address = rs.getString("address");
+        float salary = rs.getFloat("salary");
+
+        System.out.println("ID = " + id);
+        System.out.println("NAME = " + name);
+        System.out.println("AGE = " + age);
+        System.out.println("ADDRESS = " + address);
+        System.out.println("SALARY = " + salary);
+        System.out.println();
+      }
+
+      // rs.close();
+      // stmt.close();
       // c.close();
     } catch (Exception e) {
       System.err.println(e.getClass().getName() + ": " + e.getMessage());
-      System.exit(0);
     }
-    System.out.println("Table created successfully");
+
+    System.out.println("Operation done successfully");
   }
 
   public Connection connect() {
@@ -44,7 +104,6 @@ public class SQLiteJDBC {
       c = DriverManager.getConnection("jdbc:sqlite:test.db");
     } catch (Exception e) {
       System.err.println(e.getClass().getName() + ": " + e.getMessage());
-      System.exit(0);
     }
 
     System.out.println("Opened database successfully");
@@ -54,6 +113,8 @@ public class SQLiteJDBC {
   public static void main(String args[]) {
     final SQLiteJDBC inst = new SQLiteJDBC();
     inst.createTable();
+    inst.insert();
+    inst.select();
   }
 
 }
