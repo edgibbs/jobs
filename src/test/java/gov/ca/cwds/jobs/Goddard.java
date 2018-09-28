1b5d83f5f20ea8bc7652ab5857b6f839a8e5daa4
David R. Smith <David.Smith@osi.ca.gov>
2018-09-28 14:08:08 -0700

    SNAP-689: Neutron: address without start date

--- src/test/java/gov/ca/cwds/jobs/Goddard.java
+++ src/test/java/gov/ca/cwds/jobs/Goddard.java
@@ -413,7 +413,7 @@ public abstract class Goddard<T extends PersistentObject, M extends ApiGroupNorm
   }
 
   public Thread runKillThread(final BasePersonRocket<T, M> target) {
-    return runKillThread(target, 1400L);
+    return runKillThread(target, 1800L);
   }
 
   public void markTestDone() {
