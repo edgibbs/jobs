4872950303972492f600176de496b49cbea0f23b
David R. Smith <David.Smith@osi.ca.gov>
2018-10-16 10:11:42 -0700

    CANS-560: Update Jobs to use ElasticSearch 6.4.1

--- src/main/java/gov/ca/cwds/neutron/inject/ElasticUtils.java
+++ src/main/java/gov/ca/cwds/neutron/inject/ElasticUtils.java
@@ -8,31 +8,33 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import gov.ca.cwds.rest.api.ApiException;
 import org.apache.commons.lang3.StringUtils;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.elasticsearch.xpack.client.PreBuiltXPackTransportClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import gov.ca.cwds.rest.ElasticsearchConfiguration;
+import gov.ca.cwds.rest.api.ApiException;
+
 /**
- * @author Intake Team 4
+ * @author CWDS API Team
  */
 public class ElasticUtils {
+
   private static final Logger LOGGER = LoggerFactory.getLogger(ElasticUtils.class);
 
-  private ElasticUtils() {
-  }
+  private ElasticUtils() {}
 
   public static TransportClient buildElasticsearchClient(final ElasticsearchConfiguration config) {
     TransportClient client = null;
     try {
       client = makeESTransportClient(config);
 
-      for (InetSocketTransportAddress address : getValidatedESNodes(config)) {
+      for (TransportAddress address : getValidatedESNodes(config)) {
         client.addTransportAddress(address);
       }
       return client;
@@ -44,7 +46,6 @@ public class ElasticUtils {
       throw new ApiException(e);
     }
   }
-
 
   private static TransportClient makeESTransportClient(final ElasticsearchConfiguration config) {
     TransportClient client;
@@ -69,33 +70,36 @@ public class ElasticUtils {
   }
 
   @SuppressWarnings("fb-contrib:CLI_CONSTANT_LIST_INDEX")
-  private static List<InetSocketTransportAddress> getValidatedESNodes(ElasticsearchConfiguration config) {
-    final List<InetSocketTransportAddress> addressList = new ArrayList<>();
+  private static List<TransportAddress> getValidatedESNodes(ElasticsearchConfiguration config) {
+    final List<TransportAddress> addressList = new ArrayList<>();
     final List<String> nodesList = new ArrayList<>();
 
-    //If provided use Host and Port as a first node. For backward compatibility
+    // If provided use Host and Port as a first node. For backward compatibility
     if (config.getElasticsearchHost() != null && config.getElasticsearchPort() != null) {
-      nodesList.add(config.getElasticsearchHost().concat(":").concat(config.getElasticsearchPort()));
+      nodesList
+          .add(config.getElasticsearchHost().concat(":").concat(config.getElasticsearchPort()));
     }
 
-    //Comma "," separated List of host:port pairs provided in configuration file.
-    //Example: host1:port1,host2:port2,...etc.
+    // Comma "," separated List of host:port pairs provided in configuration file.
+    // Example: host1:port1,host2:port2,...etc.
     if (config.getElasticsearchNodes() != null) {
       nodesList.addAll(Arrays.asList(config.getElasticsearchNodes().split(",")));
     }
 
-    //remove duplicates if any.
+    // remove duplicates if any.
     final Map<String, String[]> nodesMap = new HashMap<>(nodesList.size());
     for (String node : nodesList) {
       nodesMap.put(node, node.split(":"));
     }
 
     for (Map.Entry<String, String[]> entry : nodesMap.entrySet()) {
-      String[] hostPort = entry.getValue();
+      final String[] hostPort = entry.getValue();
       if (hostPort.length >= 2) {
-        LOGGER.info("Adding new ES Node host:[{}] port:[{}] to elasticsearch client", hostPort[0], hostPort[1]);
+        LOGGER.info("Adding new ES Node host:[{}] port:[{}] to elasticsearch client", hostPort[0],
+            hostPort[1]);
         try {
-          addressList.add(new InetSocketTransportAddress(InetAddress.getByName(hostPort[0]), Integer.parseInt(hostPort[1])));
+          addressList.add(new TransportAddress(InetAddress.getByName(hostPort[0]),
+              Integer.parseInt(hostPort[1])));
         } catch (UnknownHostException e) {
           LOGGER.error("Error adding Node: {}", e.getMessage(), e);
         }
@@ -103,9 +107,7 @@ public class ElasticUtils {
       }
     }
 
-
     return addressList;
   }
-
 
 }
