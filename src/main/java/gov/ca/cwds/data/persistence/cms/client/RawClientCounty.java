070a81d582dec2edd3e4abf5c9badb0c9597b5ce
David R. Smith <David.Smith@osi.ca.gov>
2018-10-18 14:34:55 -0700

    SNAP-715: reduce result set columns

--- src/main/java/gov/ca/cwds/data/persistence/cms/client/RawClientCounty.java
+++ src/main/java/gov/ca/cwds/data/persistence/cms/client/RawClientCounty.java
@@ -7,17 +7,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.persistence.Column;
-import javax.persistence.Id;
 
 import org.hibernate.annotations.Type;
+
+import gov.ca.cwds.data.persistence.cms.VarargPrimaryKey;
 
 public class RawClientCounty extends ClientReference implements NeutronJdbcReader<RawClientCounty> {
 
   private static final long serialVersionUID = 1L;
-
-  @Id
-  @Column(name = "CLC_CLIENT_ID")
-  protected String clientCountyId;
 
   @Type(type = "short")
   @Column(name = "CLC_GVR_ENTC")
@@ -30,13 +27,13 @@ public class RawClientCounty extends ClientReference implements NeutronJdbcReade
   public RawClientCounty read(ResultSet rs) throws SQLException {
     super.read(rs);
     this.clientCounty = rs.getShort("CLC_GVR_ENTC");
-    this.clientCountyId = ifNull(rs.getString("CLC_CLIENT_ID"));
+    this.clientCountyRule = ifNull(rs.getString("CLC_CNTY_RULE"));
     return this;
   }
 
   @Override
   public Serializable getPrimaryKey() {
-    return clientCountyId;
+    return new VarargPrimaryKey(getCltId(), getClientCounty().toString());
   }
 
   public Short getClientCounty() {
@@ -45,14 +42,6 @@ public class RawClientCounty extends ClientReference implements NeutronJdbcReade
 
   public void setClientCounty(Short clientCounty) {
     this.clientCounty = clientCounty;
-  }
-
-  public String getClientCountyId() {
-    return clientCountyId;
-  }
-
-  public void setClientCountyId(String clientCountyId) {
-    this.clientCountyId = clientCountyId;
   }
 
   public String getClientCountyRule() {
