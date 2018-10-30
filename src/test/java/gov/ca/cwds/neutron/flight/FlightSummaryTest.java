e55e7cecd60f0226af2407cd20cf3d332c7093c1
David R. Smith <David.Smith@osi.ca.gov>
2018-10-30 16:56:28 -0700

    CANS-627: call high-level REST for ES 6.4.1 X-Pack

--- src/test/java/gov/ca/cwds/neutron/flight/FlightSummaryTest.java
+++ src/test/java/gov/ca/cwds/neutron/flight/FlightSummaryTest.java
@@ -377,7 +377,12 @@ public class FlightSummaryTest {
   public void getLastEnd_A$() throws Exception {
     Date actual = target.getLastEnd();
     Date expected = new Date();
-    assertThat(actual, is(equalTo(expected)));
+    try {
+      Thread.sleep(100); // NOSONAR
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+    }
+    assertThat(actual, is(lessThanOrEqualTo(expected)));
   }
 
   @Test
