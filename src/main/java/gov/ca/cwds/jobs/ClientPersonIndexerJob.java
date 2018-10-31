b820a68c49324e44fbdd713b2f9bccb6a787167b
David R. Smith <David.Smith@osi.ca.gov>
2018-10-31 14:10:11 -0700

    CANS-627: call high-level REST for ES 6.4.1 X-Pack

--- src/main/java/gov/ca/cwds/jobs/ClientPersonIndexerJob.java
+++ src/main/java/gov/ca/cwds/jobs/ClientPersonIndexerJob.java
@@ -25,7 +25,7 @@ import gov.ca.cwds.dao.cms.ReplicatedClientDao;
 import gov.ca.cwds.data.es.ElasticSearchPerson;
 import gov.ca.cwds.data.es.ElasticSearchPerson.ESOptionalCollection;
 import gov.ca.cwds.data.es.ElasticSearchPersonAddress;
-import gov.ca.cwds.data.es.ElasticsearchDao;
+import gov.ca.cwds.data.es.NeutronElasticSearchDao;
 import gov.ca.cwds.data.persistence.PersistentObject;
 import gov.ca.cwds.data.persistence.cms.client.RawClient;
 import gov.ca.cwds.data.persistence.cms.rep.ReplicatedAddress;
@@ -93,7 +93,7 @@ public class ClientPersonIndexerJob extends InitialLoadJdbcRocket<ReplicatedClie
    */
   @Inject
   public ClientPersonIndexerJob(final ReplicatedClientDao dao,
-      @Named("elasticsearch.dao.people-summary") final ElasticsearchDao esDao,
+      @Named("elasticsearch.dao.people-summary") final NeutronElasticSearchDao esDao,
       @LastRunFile final String lastRunFile, final ObjectMapper mapper, FlightPlan flightPlan,
       AtomLaunchDirector launchDirector) {
     super(dao, esDao, lastRunFile, mapper, flightPlan, launchDirector);
