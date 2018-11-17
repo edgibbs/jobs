9e29e705dc798a6295bf34449a3917a0f74a05e4
David R. Smith <David.Smith@osi.ca.gov>
2018-11-17 15:20:40 -0800

    SNAP-754: remove deleted clients from index

--- src/main/java/gov/ca/cwds/neutron/util/jdbc/NeutronDB2Utils.java
+++ src/main/java/gov/ca/cwds/neutron/util/jdbc/NeutronDB2Utils.java
@@ -43,7 +43,7 @@ public class NeutronDB2Utils {
     final String strEndDate = NeutronDateUtils.makeSimpleTimestampString(lastRunEndDate);
     return sql.replaceAll("XYZ", strStartDate).replaceAll("LAST_RUN_START", strStartDate)
         .replaceAll("LAST_RUN_END", strEndDate)
-        .replaceAll("'CURRENT TIMESTAMP'", "CURRENT TIMESTAMP")
+        .replaceAll("'CURRENT TIMESTAMP'", "CURRENT TIMESTAMP") // remove single quotes
         .replaceAll("LAST_RUN_DATE", strLastRunDate);
   }
 
