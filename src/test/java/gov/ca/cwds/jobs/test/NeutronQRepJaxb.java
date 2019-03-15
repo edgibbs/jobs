7665c1c28149d523014d0ae61b3ca3ddf41a99c8
David R. Smith <David.Smith@osi.ca.gov>
2019-03-15 11:32:24 -0700

    SNAP-859: MQ replication

--- src/test/java/gov/ca/cwds/jobs/test/NeutronQRepJaxb.java
+++ src/test/java/gov/ca/cwds/jobs/test/NeutronQRepJaxb.java
@@ -1,33 +1,51 @@
 package gov.ca.cwds.jobs.test;
 
-import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.Reader;
+import java.io.StringReader;
+import java.nio.charset.Charset;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 import javax.xml.bind.JAXBIntrospector;
 import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
+
+import org.apache.commons.io.IOUtils;
 
 import mqcap.xsd.MsgType;
 import mqcap.xsd.TransType;
 
 public class NeutronQRepJaxb {
 
-  public NeutronQRepJaxb() {
+  public NeutronQRepJaxb() {}
 
+  public MsgType read(String xmlLocation) throws JAXBException, IOException {
+    MsgType ret = null;
+    final JAXBContext jc = JAXBContext.newInstance(TransType.class.getPackage().getName());
+    try (final Reader r =
+        new StringReader(IOUtils.resourceToString(xmlLocation, Charset.defaultCharset()));) {
+      ret = (MsgType) JAXBIntrospector.getValue(jc.createUnmarshaller().unmarshal(r));
+    } finally {
+      // auto-close reader
+    }
+
+    return ret;
+  }
+
+  public void write(MsgType msg, PrintStream out) throws JAXBException, IOException {
+    final JAXBContext jc = JAXBContext.newInstance(TransType.class.getPackage().getName());
+    final Marshaller m = jc.createMarshaller();
+
+    m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+    m.setProperty(Marshaller.JAXB_NO_NAMESPACE_SCHEMA_LOCATION, "/qrep/mqcap/mqcap.xsd");
+    m.marshal(msg, out);
   }
 
   public static void main(String[] args) throws Exception {
-    final JAXBContext jc = JAXBContext.newInstance(TransType.class.getPackage().getName());
-    final Unmarshaller unmarshaller = jc.createUnmarshaller();
-    final Marshaller m = jc.createMarshaller();
-
-    final File xml = new File("/Users/dsmith/cws_legacy/db2/q_rep/repl/q/test.xml");
-    final MsgType config = (MsgType) JAXBIntrospector.getValue(unmarshaller.unmarshal(xml));
-
-    m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-    m.setProperty(Marshaller.JAXB_NO_NAMESPACE_SCHEMA_LOCATION,
-        "/Users/dsmith/cws_legacy/db2/q_rep/repl/q/mqcap.xsd");
-    m.marshal(config, System.out);
+    final NeutronQRepJaxb inst = new NeutronQRepJaxb();
+    final MsgType msg = inst.read("/qrep/mqcap/test.xml");
+    inst.write(msg, System.out);
   }
 
 }
