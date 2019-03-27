bc9f9aad2d0de40d1d86193a9056553fa34c5c11
David R. Smith <David.Smith@osi.ca.gov>
2019-03-27 11:12:30 -0700

    SNAP-925: DGGT

--- src/test/java/gov/ca/cwds/jobs/test/ReadJSONExample.java
+++ src/test/java/gov/ca/cwds/jobs/test/ReadJSONExample.java
@@ -1,8 +1,6 @@
 package gov.ca.cwds.jobs.test;
 
 import java.io.FileReader;
-import java.util.Iterator;
-import java.util.Map;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -16,41 +14,45 @@ public class ReadJSONExample {
 
     // typecasting obj to JSONObject
     final JSONObject jo = (JSONObject) obj;
+    final JSONObject outerHits = (JSONObject) jo.get("hits");
 
     // getting firstName and lastName
-    final String firstName = (String) jo.get("firstName");
-    final String lastName = (String) jo.get("lastName");
-
-    System.out.println(firstName);
-    System.out.println(lastName);
-
-    // getting age
-    final long age = (long) jo.get("age");
-    System.out.println(age);
+    // final String firstName = (String) jo.get("firstName");
+    // final String lastName = (String) jo.get("lastName");
+    //
+    // System.out.println(firstName);
+    // System.out.println(lastName);
+    //
+    // // getting age
+    // final Long age = (Long) jo.get("age");
+    // System.out.println(age);
 
     // getting address
-    final Map address = ((Map) jo.get("address"));
-
-    // iterating address Map
-    Iterator<Map.Entry> itr1 = address.entrySet().iterator();
-    while (itr1.hasNext()) {
-      Map.Entry pair = itr1.next();
-      System.out.println(pair.getKey() + " : " + pair.getValue());
-    }
+    // final Map hits = ((Map) outerHits.get("hits"));
+    //
+    // // iterating address Map
+    // Iterator<Map.Entry> itr1 = hits.entrySet().iterator();
+    // while (itr1.hasNext()) {
+    // Map.Entry pair = itr1.next();
+    // System.out.println(pair.getKey() + " : " + pair.getValue());
+    // }
 
     // getting phoneNumbers
-    final JSONArray ja = (JSONArray) jo.get("phoneNumbers");
+    final JSONArray hits = (JSONArray) outerHits.get("hits");
+    System.out.println(hits);
 
-    // iterating phoneNumbers
-    Iterator itr2 = ja.iterator();
+    // // iterating phoneNumbers
+    // Iterator itr2 = ja.iterator();
+    // Iterator<Map.Entry> itr1;
+    //
+    // while (itr2.hasNext()) {
+    // itr1 = ((Map) itr2.next()).entrySet().iterator();
+    // while (itr1.hasNext()) {
+    // Map.Entry pair = itr1.next();
+    // System.out.println(pair.getKey() + " : " + pair.getValue());
+    // }
+    // }
 
-    while (itr2.hasNext()) {
-      itr1 = ((Map) itr2.next()).entrySet().iterator();
-      while (itr1.hasNext()) {
-        Map.Entry pair = itr1.next();
-        System.out.println(pair.getKey() + " : " + pair.getValue());
-      }
-    }
   }
 
 }
