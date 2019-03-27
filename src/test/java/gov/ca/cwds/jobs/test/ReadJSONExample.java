536d67d8bd0c25ded4dda1b16520b68ed0540b52
David R. Smith <David.Smith@osi.ca.gov>
2019-03-27 11:29:31 -0700

    SNAP-925: DGGT

--- src/test/java/gov/ca/cwds/jobs/test/ReadJSONExample.java
+++ src/test/java/gov/ca/cwds/jobs/test/ReadJSONExample.java
@@ -1,6 +1,8 @@
 package gov.ca.cwds.jobs.test;
 
 import java.io.FileReader;
+import java.util.Iterator;
+import java.util.Map;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -11,47 +13,30 @@ public class ReadJSONExample {
 
   public static void main(String[] args) throws Exception {
     final Object obj = new JSONParser().parse(new FileReader(args[0]));
-
-    // typecasting obj to JSONObject
     final JSONObject jo = (JSONObject) obj;
     final JSONObject outerHits = (JSONObject) jo.get("hits");
-
-    // getting firstName and lastName
-    // final String firstName = (String) jo.get("firstName");
-    // final String lastName = (String) jo.get("lastName");
-    //
-    // System.out.println(firstName);
-    // System.out.println(lastName);
-    //
-    // // getting age
-    // final Long age = (Long) jo.get("age");
-    // System.out.println(age);
-
-    // getting address
-    // final Map hits = ((Map) outerHits.get("hits"));
-    //
-    // // iterating address Map
-    // Iterator<Map.Entry> itr1 = hits.entrySet().iterator();
-    // while (itr1.hasNext()) {
-    // Map.Entry pair = itr1.next();
-    // System.out.println(pair.getKey() + " : " + pair.getValue());
-    // }
-
-    // getting phoneNumbers
     final JSONArray hits = (JSONArray) outerHits.get("hits");
-    System.out.println(hits);
+    // System.out.println(hits);
 
-    // // iterating phoneNumbers
-    // Iterator itr2 = ja.iterator();
-    // Iterator<Map.Entry> itr1;
-    //
-    // while (itr2.hasNext()) {
-    // itr1 = ((Map) itr2.next()).entrySet().iterator();
-    // while (itr1.hasNext()) {
-    // Map.Entry pair = itr1.next();
-    // System.out.println(pair.getKey() + " : " + pair.getValue());
-    // }
-    // }
+    // Iterate hits:
+    Iterator iter = hits.iterator();
+    Iterator<Map.Entry> itr1;
+
+    while (iter.hasNext()) {
+      final Map map = (Map) iter.next();
+      final JSONArray matchedQueries = (JSONArray) map.get("matched_queries");
+      System.out.println(matchedQueries);
+
+      // itr1 = ((Map) iter.next()).entrySet().iterator();
+      // while (itr1.hasNext()) {
+      // Map.Entry pair = itr1.next();
+      // System.out.println(pair.getKey() + " : " + pair.getValue());
+      // System.out.println(pair.getValue().getClass());
+      // }
+    }
+
+    // class org.json.simple.JSONObject
+    // Collections.sort(hits, c);
 
   }
 
